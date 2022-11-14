cb599b58c8226f6e4fefeb8501b17f93613e04e1
Алексей Н. Мельников <u183@stekspb.ru>
2022-11-14 17:27:10 +0300

    ajax

--- meadowlark.js
+++ meadowlark.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const multiparty = require("multiparty");
 
 const handlers = require("./lib/handlers");
 
 const app = express();
 
+// Удаляем заголовок ответа сервера (небольшая защита против хакеров)
+app.disable("x-powered-by");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+// Это необходимо, чтобы парсить веб-формы
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -16,15 +25,43 @@ app.get("/", handlers.home);
 
 app.get("/about", handlers.about);
 
+app.get("/headers", (req, res) => {
+  console.log(req.originalUrl);
+  res.type("text/plain");
+  const headers = Object.entries(req.headers).map(
+    ([key, value]) => `${key}: ${value}`
+  );
+  res.send(headers.join("\n"));
+});
+
+app.get("/newsletter", handlers.newsletter);
+
+app.post("/api/newsletter-signup", handlers.api.newsletterSignup);
+
+app.get("/contest/vacation-photo-ajax", handlers.vacationPhotoContestAjax);
+
+app.post("/api/vacation-photo-contest/:year/:month", (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err)
+      return handlers.api.vacationPhotoContestError(req, res, err.message);
+    handlers.api.vacationPhotoContest(req, res, fields, files);
+  });
+});
+
 // custom 404 page
 app.use(handlers.notFound);
 
 // custom 500 page
 app.use(handlers.serverError);
 
-app.listen(port, () =>
-  console.log(
-    `Express started on http://localhost:${port}; ` +
-      `press Ctrl-C to terminate.`
-  )
-);
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(
+      `Express started on http://localhost:${port}` +
+        "; press Ctrl-C to terminate."
+    );
+  });
+} else {
+  module.exports = app;
+}
