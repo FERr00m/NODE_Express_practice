88e087dd0b5ddf1f2db32dccdf21c4afaec39ee5
Алексей Н. Мельников <u183@stekspb.ru>
2022-11-15 12:57:17 +0300

    add widget styles scripts

--- bs-config.js
+++ bs-config.js
@@ -2,7 +2,7 @@ module.exports = {
   proxy: "localhost:8000",
   files: ["**/*.css", "**/*.pug", "**/*.js"],
   ignore: ["node_modules"],
-  reloadDelay: 10,
+  reloadDelay: 3,
   ui: false,
   notify: false,
   port: 3000,
--- meadowlark.js
+++ meadowlark.js
@@ -2,8 +2,13 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const multiparty = require("multiparty");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
 
 const handlers = require("./lib/handlers");
+const weatherMiddlware = require("./lib/middleware/weather");
+const flashMiddleware = require("./lib/middleware/flash");
+const { credentials } = require("./config");
 
 const app = express();
 
@@ -17,7 +22,18 @@ app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(cookieParser(credentials.cookieSecret));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+  })
+);
+
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(weatherMiddlware);
+app.use(flashMiddleware);
 
 const port = process.env.PORT || 8000;
 
